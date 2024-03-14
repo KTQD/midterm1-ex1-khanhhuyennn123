68d16f876d234c7a1a3a561e25110a9606a08e47
khanhhuyennn123 <157661741+khanhhuyennn123@users.noreply.github.com>
2024-03-14 08:05:49 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("Sitde", "POP", "CS64", 100.0f, 116);
+        Book book = new Book("Java", "Lap trinh da luong", "T5_12", 35.0f, "KhanhHuyen", 5, "A");
 
-        MP3 mp3;
+        System.out.println("MP3 Information:");
         mp3.showInfo();
 
-        Book book;
+        System.out.println("\nBook Information:");
         book.showInfo();
-
     }
-}
