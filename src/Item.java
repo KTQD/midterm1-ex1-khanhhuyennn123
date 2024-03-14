e7a61658146a3763525f81d9aeec38c02bb55354
khanhhuyennn123 <157661741+khanhhuyennn123@users.noreply.github.com>
2024-03-14 08:04:17 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,12 @@
-public class Item {
-}
+abstract class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    float price;
+
+    public Item(String name, String description, String ID, float price) {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
